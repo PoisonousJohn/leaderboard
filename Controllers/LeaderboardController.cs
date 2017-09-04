5fd5f00c5ee9cf022c517c672b26cf663e7f09a3
Ivan Fateev <ivan.fateev@microsoft.com>
2017-09-04 10:34:27 +0300

    set up controller and swagger integration

---
+++ Controllers/LeaderboardController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace leaderboard.Controllers
+{
+    [Route("api/[controller]")]
+    public class LeaderboardController : Controller
+    {
+        // GET api/values
+        [HttpGet("{leaderboardName}")]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace leaderboard
 {
@@ -17,12 +18,22 @@ namespace leaderboard
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Leaderboard API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             app.UseMvc();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Leaderboard API V1");
+            });
         }
     }
 }
