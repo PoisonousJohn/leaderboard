d526f2cbb9e0934a96af3534f9409dd97ee06cb0
Ivan Fateev <ivan.fateev@microsoft.com>
2017-09-04 12:12:56 +0300

    added redis integration

--- Controllers/LeaderboardController.cs
+++ Controllers/LeaderboardController.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using ServiceStack.Redis;
 
 namespace leaderboard.Controllers
 {
     [Route("api/[controller]")]
     public class LeaderboardController : Controller
     {
+
+        public LeaderboardController(IRedisClientsManager redis)
+        {
+            _redis = redis.GetClient();
+        }
+
         // GET api/values
         [HttpGet("{leaderboardName}")]
         public IEnumerable<string> Get()
@@ -16,5 +24,6 @@ namespace leaderboard.Controllers
             return new string[] { "value1", "value2" };
         }
 
+        private readonly IRedisClient _redis;
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -8,26 +8,46 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using ServiceStack.Redis;
 using Swashbuckle.AspNetCore.Swagger;
 
 namespace leaderboard
 {
     public class Startup
     {
+        public static IConfigurationRoot Configuration { get; set; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
 
+            services.AddDistributedRedisCache(o => {
+                o.Configuration = "localhost";
+                o.InstanceName = "SampleInstance";
+            });
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new Info { Title = "Leaderboard API", Version = "v1" });
             });
+            services.Add(new ServiceDescriptor(
+                typeof(IRedisClientsManager),
+                c => new BasicRedisClientManager(Configuration.GetConnectionString("redis")),
+                ServiceLifetime.Singleton
+            ));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            // https://docs.microsoft.com/en-us/aspnet/core/fundamentals/configuration
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
             app.UseMvc();
             app.UseSwagger();
             app.UseSwaggerUI(c =>
@@ -35,5 +55,6 @@ namespace leaderboard
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "Leaderboard API V1");
             });
         }
+
     }
 }
